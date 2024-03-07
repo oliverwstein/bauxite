20b81a7cc6ea826b104bc33d9990789badcfe360
oliverwstein <oliversteininc@gmail.com>
2024-03-07 09:31:09 -0500

    Switched from tiny http to Rocket.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,16 @@
-use tiny_http::{Server, Response};
+#[macro_use] extern crate rocket;
 
-fn main() {
-    // Set the address and port for the server
-    let server = Server::http("0.0.0.0:8000").unwrap();
-
-    println!("Server running on http://0.0.0.0:8000");
-
-    // Serve requests in a loop
-    for request in server.incoming_requests() {
-        let response = Response::from_string("
-            <!DOCTYPE html>
-            <html lang=\"en\">
-            <head>
-                <meta charset=\"UTF-8\">
-                <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-                <title>Simple Rust Webpage</title>
-            </head>
-            <body>
-                <h1>Hello from Rust!</h1>
-                <p>This is a basic web page served using Rust.</p>
-            </body>
-            </html>
-        ");
-
-        // Send the response to the client
-        let _ = request.respond(response);
-    }
+#[get("/")]
+fn index() -> &'static str {
+    "<h1>Welcome to My Rust Webpage</h1><p>This is the splash page.</p><a href=\"/about\">About Me</a>"
 }
 
+#[get("/about")]
+fn about() -> &'static str {
+    "<h1>About Me</h1><p>This is the About Me page.</p>"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, about])
+}
