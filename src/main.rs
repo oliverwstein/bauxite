ec0b20aa890165f9faeee5f0814aa9f2ead63952
oliverwstein <oliversteininc@gmail.com>
2024-03-07 09:57:14 -0500

    Basic HTML version

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 #[macro_use] extern crate rocket;
+use rocket::response::content::RawHtml;
 
 #[get("/")]
-fn index() -> &'static str {
-    "<h1>Welcome to My Rust Webpage</h1><p>This is the splash page.</p><a href=\"/about\">About Me</a>"
+fn index() -> RawHtml<String> {
+    RawHtml(format!(
+        "<nav><a href=\"/\">Home</a> | <a href=\"/about\">About Me</a></nav>\
+        <h1>Welcome to My Rust Webpage</h1>\
+        <p>This is the splash page.</p>"
+    ))
 }
 
 #[get("/about")]
-fn about() -> &'static str {
-    "<h1>About Me</h1><p>This is the About Me page.</p>"
+fn about() -> RawHtml<String> {
+    RawHtml(format!(
+        "<nav><a href=\"/\">Home</a> | <a href=\"/about\">About Me</a></nav>\
+        <h1>About Me</h1>\
+        <p>This is the About Me page.</p>"
+    ))
 }
 
 #[launch]
